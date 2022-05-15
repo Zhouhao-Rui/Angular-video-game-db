aef40fe9db7c5d91313ad2d4e55484cada912aa1
Zhouhao-Rui <976667412@qq.com>
2022-05-15 18:46:23 +0100

    Init

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { GaugeModule } from "angular-gauge";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatIconModule } from "@angular/material/icon";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    GaugeModule.forRoot(),
+    MatFormFieldModule,
+    MatIconModule,
+    MatSelectModule,
+    MatTabsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
